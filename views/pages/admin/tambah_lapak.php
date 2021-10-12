6d6283439a7a8dd54a4a5c54037d1c423bdb9898
ihsanfj55 <ihsanfj55@gmail.com>
2021-10-12 22:37:47 +0800

    edit input lapak

--- views/pages/admin/tambah_lapak.php
+++ views/pages/admin/tambah_lapak.php
@@ -9,7 +9,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="<?= $base_url ?>beranda_admin">Kampung Kuliner</a></li>
-                        <li class="breadcrumb-item active">Beranda</li>
+                        <li class="breadcrumb-item active">Tambah Lapak</li>
                     </ol>
                 </div>
             </div>
@@ -20,20 +20,43 @@
     <section class="content">
 
         <!-- Default box -->
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Title</h3>
+        <!-- Datatables -->
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h5 class="m-0 font-weight-bold text-primary">Form Tambah Lapak</h5>
+                </div>
+                <div class="card-body">
+                    <form action="aksi/tambah_berita.php" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="judul">Nama Lapak</label>
+                            <input type="text" class="form-control" id="nama_lapak" aria-describedby="nama_lapak" name="nama_lapak">
+                        </div>
+                        <div class="form-group">
+                            <label for="isi">Deskripsi</label>
+                            <textarea type="text" class="form-control" id="deskripsi_lapak" placeholder="" rows="5" name="deskripsi_lapak"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="gambar">File Gambar</label>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="customFile" name="gambar">
+                                <label class="custom-file-label" for="customFile">Choose file</label>
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <label for="">Nomor Telepon</label>
+                            <input type="number" class="form-control" id="no_telp" name="no_telp">
+                        </div>
+                        <div class="mb-4">
+                            <label for="">Nomor WhatsApp</label>
+                            <input type="number" class="form-control" id="no_wa" name="no_wa">
+                        </div>
+                        <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+                    </form>
+                </div>
             </div>
-            <div class="card-body">
-                Start creating your amazing application! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque expedita quo ullam quas alias numquam quis quasi praesentium deserunt repellendus corporis at consectetur, quod ad vitae quaerat veritatis accusamus enim sapiente fugit reprehenderit fuga laudantium sint? Quam aspernatur quo, ducimus sit quia quidem culpa labore perspiciatis totam, nam neque odit! Dolorum earum laborum doloremque vero fuga similique dolor at repudiandae eos unde laboriosam, assumenda atque aliquam alias mollitia necessitatibus totam voluptates quo, aut doloribus est sequi consectetur. Reiciendis cupiditate, velit dolore labore saepe perspiciatis? Similique alias dignissimos itaque ab! Delectus maiores perspiciatis, dolore rerum culpa minus ad architecto totam blanditiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores hic dolore error, necessitatibus doloremque sequi porro assumenda nostrum consectetur ipsa voluptatem sint quos quam consequuntur quo molestiae, facilis placeat incidunt tenetur. Dolores dignissimos repellendus reiciendis minus commodi mollitia harum neque atque necessitatibus maxime! Repudiandae repellat quia in sed sapiente voluptatum ab molestias consequatur vitae, atque eius beatae ipsam totam explicabo? Unde culpa at explicabo dolor minus qui molestias eligendi in. Odit cumque officiis porro placeat tempora eos, et cum id veritatis velit numquam perferendis repudiandae commodi? Animi, iure ea eos a saepe nulla quidem repellat nostrum incidunt odit! Neque, vitae! Nostrum vero corporis, aliquam veniam ipsum est! Odio cumque aliquam cupiditate labore inventore maiores, quo, deserunt consectetur dolore repellendus sunt nobis. Mollitia laudantium quia tempore eos, enim accusamus, ipsam necessitatibus molestias reiciendis nemo ratione quos! Inventore laborum aperiam, et dignissimos in cupiditate architecto ullam tenetur sequi similique quasi illo non repellat eligendi sed assumenda fugit quo asperiores, quibusdam est voluptates suscipit officiis totam? Iure maiores debitis vitae architecto, error repellat ipsa, quaerat magni impedit dolor repellendus quos nostrum molestias odio repudiandae quis deleniti. Similique, nesciunt officiis! Nostrum quod nemo, velit repellat veniam perspiciatis dolore esse? Harum beatae perspiciatis quos corporis? Consequuntur numquam veritatis voluptas fuga placeat aut quis inventore labore aspernatur sed dignissimos cumque pariatur excepturi nemo saepe quasi, quo unde quas modi quae animi! Vero nihil incidunt possimus illum pariatur nemo omnis, quo ea alias aperiam ipsa, deserunt reprehenderit, velit enim recusandae maxime odit porro laudantium officiis eligendi debitis ex? Quae optio ducimus quidem autem perferendis tempore ut sequi voluptates eius quibusdam reiciendis illo, non in veniam minus animi consequatur, quaerat dicta labore id placeat soluta? Dicta iure dolorem quia sapiente molestias minus eligendi suscipit obcaecati maiores quis enim quaerat ipsa sunt, officia est culpa fuga soluta dolores totam nostrum facere veritatis sit? Quis officiis eaque officia atque? Nulla corporis sed fugit molestias iusto, architecto tempora, doloremque dicta quia, ipsa placeat explicabo a voluptatibus. Neque ducimus sequi accusantium molestias mollitia nam ipsum praesentium facere assumenda, modi necessitatibus, ab adipisci laboriosam fugiat illum, dolorem repellat fuga animi nobis qui totam veritatis voluptas. Quis, cumque facere sequi laborum sapiente provident molestiae ullam atque eveniet sed corrupti iure impedit, illo vitae magnam eos fugit tempore et similique aut libero cum voluptas! Labore incidunt ipsa nobis, eos magnam expedita sapiente eveniet, ducimus natus rerum, nesciunt officiis quod! Eum, consequuntur perspiciatis ullam minus doloribus maiores eligendi mollitia quod odio est repellat voluptas. Eaque dolorum iure tempore quibusdam error? Quis sed accusantium possimus atque laudantium odio quae numquam expedita explicabo optio fugiat mollitia eaque repellat minima, blanditiis nihil iusto temporibus ab similique quaerat eius! Soluta minima sed excepturi, fuga sunt, minus harum perferendis incidunt animi praesentium fugit error, odio id magni consectetur dignissimos. Voluptas sunt itaque minus magnam earum accusantium numquam totam similique odit ipsum quam ipsam in delectus hic, culpa, velit fugit. Ipsum, corporis ad unde consectetur voluptatibus vel cupiditate facere ea quos optio exercitationem repellendus quam veniam quibusdam ipsam architecto quia maiores ab?
-            </div>
-            <!-- /.card-body -->
-            <div class="card-footer">
-                Footer
-            </div>
-            <!-- /.card-footer-->
         </div>
-        <!-- /.card -->
+
 
     </section>
     <!-- /.content -->
